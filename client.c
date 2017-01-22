88c89e250d6c012f444b0dbcaa14655877eec72b
Pedram Pejman <pedrampejman2010@gmail.com>
2017-01-22 18:17:26 -0500

    Moved from complete project

---
+++ client.c
@@ -0,0 +1,302 @@
+#include "client.h"
+
+/*
+** Establishes a TCP connection to server on specified port
+** Returns file descriptor for socket or -1 for error and writes error 
+** error description in the error buffer.
+*/
+int connect_to_server(char *server, int PORT, char *error) {
+  // Interpret server address
+  struct hostent *host_info = gethostbyname(server);
+  if (host_info == NULL) {
+    sprintf(error, "Could not look up server address");
+    return -1;
+  }
+
+  // Create socket to connect to the server
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    sprintf(error, "Failed obtaining a socket file descriptor");
+    return -1;
+  }
+
+  // Initialize socket address struct 
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = host_info->h_addrtype;
+  memcpy((char *)&server_addr.sin_addr.s_addr, host_info->h_addr_list[0],
+         host_info->h_length);
+  server_addr.sin_port = htons(PORT);
+
+  // Try to connect to the server
+  if (connect(socket_fd, (struct sockaddr *)&server_addr,
+              sizeof(server_addr)) < 0) {
+    sprintf(error, "Cannot connect to server");
+    return -1;
+  }
+  printf("Succesfully connected to the server\n");
+
+  return socket_fd;
+}
+
+/*
+** If ack is an ack_code, echos its message, otherwise echos 
+** the content of the message.
+** Returns an AckResult enum member
+*/
+int handle_ack(char *ack) {
+  if (!strncmp(ack, REQ_AOK, strlen(REQ_AOK))) {
+    printf(REQ_AOK_MSG);
+  } else if (!strncmp(ack, INV_USR, strlen(INV_USR))){
+    printf(INV_USR_MSG);
+    return TERMINATE;
+  } else if (!strncmp(ack, INV_MOD, strlen(INV_MOD))){
+    printf(INV_MOD_MSG);
+    return TERMINATE;
+  } else if (!strncmp(ack, UNK_ERR, strlen(UNK_ERR))){
+    printf(UNK_ERR_MSG);
+    return TERMINATE;
+  } else if (!strncmp(ack, CHK_ERR, strlen(CHK_ERR))){
+    printf(CHK_ERR_MSG);
+    return TERMINATE;
+  } else if (!strncmp(ack, TIM_OUT, strlen(TIM_OUT))){
+    printf(TIM_OUT_MSG);
+    return TERMINATE;
+  } else if (!strncmp(ack, CMP_AOK, strlen(CMP_AOK))){
+    printf(CMP_AOK_MSG);
+  } else if (!strncmp(ack, CMP_ERR, strlen(CMP_ERR))){
+    printf(CMP_ERR_MSG);
+  } else if (!strncmp(ack, RUN_AOK, strlen(RUN_AOK))){
+    printf(RUN_AOK_MSG);
+  } else if (!strncmp(ack, RUN_ERR, strlen(RUN_ERR))){
+    printf(RUN_ERR_MSG);
+  } else if (!strncmp(ack, CHK_AOK, strlen(CHK_AOK))){
+    printf(CHK_AOK_MSG);
+  } else if (!strncmp(ack, JDG_AOK, strlen(JDG_AOK))){
+    printf(JDG_AOK_MSG);
+  } else if (!strncmp(ack, JDG_ERR, strlen(JDG_ERR))){
+    printf(JDG_ERR_MSG);
+  } else if (!strncmp(ack, BEG_FIL, strlen(BEG_FIL))) {
+    printf(BEG_FIL_MSG);
+  } else if (!strncmp(ack, BEG_SOL, strlen(BEG_SOL))){
+    return RECEIVE_SOLUTION;
+  } else {
+    printf("%s%s%s", KNRM, ack, KYEL);
+  }
+    return NO_ACTION;
+}
+
+/*
+** Parses the header of the request and returns an array of tokens
+** Returns the number of tokens parsed
+*/
+int parse_arguments(char **args, char *line) {
+  int i = 0;
+  args[i] = strtok(line, ARG_DELIM);
+  while ((args[++i] = strtok(NULL, ARG_DELIM)) != NULL);
+  return i - 1;
+}
+
+/*
+** Receives the solution to the current module from the server
+** Saves the file in the current directory
+*/
+void receive_solution(int socket_fd) {
+
+  // Receive the header_tokens and write the file
+  char header_buffer[TCP_PACKET_SIZE];
+  if (recv(socket_fd, header_buffer, sizeof(header_buffer), 0)) {
+    char *header_tokens[MAX_HEADER_ELEMS];
+    char module_num[MAX_FILENAME_LEN];
+    int bytes_remaining, bytes_received;
+
+    // Make sure the first packet is the header data
+    if (!strncmp(header_buffer, HEADER_PREFIX, strlen(HEADER_PREFIX))) {
+
+      // Parse the header and capture data
+      parse_arguments(header_tokens, header_buffer);
+      strcpy(module_num, header_tokens[1]);	
+      bytes_remaining = atoi(header_tokens[2]);
+      
+      // Send header received ack
+      send(socket_fd, HDR_AOK, strlen(HDR_AOK), 0);
+
+    } else {
+      // Print error message and exit
+      printf("Failed to receive instructor solution received [%s]\n",
+	     header_buffer);
+      close(socket_fd);
+      return 1;
+    }
+
+    char filepath[MAX_FILENAME_LEN];
+    sprintf(filepath, "%s_%s_%s", INSTR, module_num, SOL_SUFFIX);
+
+    // Open file to write to and
+    FILE *file_to_write = open(filepath, O_WRONLY | O_TRUNC | O_CREAT, 00664);
+
+    int total_received=0;
+    char receive_buffer[TCP_PACKET_SIZE];
+
+    // Listen until file is received or no more packets are being sent
+    while ((bytes_remaining > 0) && 
+        ((bytes_received = recv(socket_fd, receive_buffer, TCP_PACKET_SIZE, 0)) > 0)) {
+
+      // Write the received packet of data to the file
+      write(file_to_write, receive_buffer, bytes_received);
+      total_received += bytes_received;
+      bytes_remaining -= bytes_received;
+    }
+
+    // Make sure we received the number of bytes we expected
+    if (!bytes_remaining) {
+      send(socket_fd, FIL_AOK, strlen(FIL_AOK), 0);
+      printf("Succesfully retrieved instructor's solution file - "
+        "Saved in current directory\n");
+    } else {
+      send(socket_fd, FIL_ERR, strlen(FIL_ERR), 0);
+    }
+
+    close(file_to_write);
+
+  } else {
+    printf("Server dropped connection\n\n");
+  }
+
+}
+
+/*
+** Sends the request packet {username, module number, source file}
+** to the server over socket_fd. It then waits for the server to send
+** back acknowledgement messages, echoing them as they are received.
+** It halts when server closes the connection.
+** TODO: Break up into 2 loosely coupled routines.
+*/
+int send_request(int socket_fd, char *lfile, char *user, char *module_num) {
+
+  // Open file to send
+  FILE *file_to_send = fopen(lfile, "r");
+  if (!file_to_send) {
+    printf("Error opening file\n");
+    close(socket_fd);
+    return 1;
+  }
+
+  // Compute file size
+  long file_size;
+  fseek(file_to_send, 0, SEEK_END);
+  file_size = ftell(file_to_send);
+  rewind(file_to_send);
+  if (file_size > 1024)
+    printf("Reading content of %s (%d Kb)\n", lfile, file_size/1024);
+  else 
+    printf("Reading content of %s (%d bytes)\n", lfile, file_size);
+  
+  // Write and send request header
+  char write_buffer[TCP_PACKET_SIZE];
+  sprintf(write_buffer, "%s%s%s%s%s%s%d\r\n", HEADER_PREFIX, ARG_DELIM, user,
+          ARG_DELIM, module_num, ARG_DELIM, file_size);
+  send(socket_fd, write_buffer, sizeof(write_buffer), 0);
+
+
+  // Wait for header to be received
+  memset(&write_buffer, 0, TCP_PACKET_SIZE);
+  recv(socket_fd, write_buffer, TCP_PACKET_SIZE, 0);
+  if (strcmp(write_buffer, HDR_AOK)) {
+    printf("Error sending request to server\n");
+    fclose(file_to_send);
+    close(socket_fd);
+    return 1;  
+  }
+
+  int remaining = file_size, buff_size = 0, bytes_read;
+  printf("Sending file content...\n");
+
+  // Send the entire file over the socket_fd
+  while (remaining > 0) {
+    bytes_read = fread(&write_buffer, 1, sizeof(write_buffer), file_to_send);
+
+    // Exit if read resulted in error
+    if (bytes_read < 0) {
+      perror("Error reading from file:");
+      fclose(file_to_send);
+      close(socket_fd);
+      return 1;
+    }
+
+    // Send the data read
+    if (remaining >= sizeof(write_buffer))
+      buff_size = sizeof(write_buffer);
+    else
+      buff_size = remaining;
+    send(socket_fd, write_buffer, buff_size, 0);
+    remaining -= buff_size;
+  }
+
+  printf("Finished sending file content\nWaiting for feedback from server...\n");
+
+  char read_buffer[TCP_PACKET_SIZE];
+
+  // Receive status updates (or error messages) until connection is closed or handle_ack
+  // signals a force exit.
+  int ack_result;
+  while ((recv(socket_fd, read_buffer, ACK_LEN, 0) > 0)) {
+    ack_result = handle_ack(&read_buffer);
+
+    if (ack_result == TERMINATE) 
+      break;
+    else if (ack_result == RECEIVE_SOLUTION) 
+      receive_solution(socket_fd);
+
+    memset(read_buffer, 0, TCP_PACKET_SIZE);
+  }
+
+  printf("Connection closed - Exiting\n");
+  fclose(file_to_send);
+
+  return 0;
+}
+
+void fatal_error(char *message) {
+    if (message) printf("fatal: %s\n", message);
+    printf(KNRM);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+
+  // Make sure we got the right number of arguments
+  if (argc != 4) {
+    printf("USAGE: %s <Username> <Module number> <File to submit>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // Change terminal text color to yellow
+  printf(KYEL);
+
+  // Set request parameters
+  const char *user = argv[1];
+  const char *module_num = argv[2];
+  const char *file_to_send = argv[3];
+
+  // Establish a TCP connection to the server
+  char error_message[MAX_ERROR_SIZE];
+  int socket_fd = connect_to_server(server_address, port_number, error_message);
+ 
+  if (socket_fd  < 0) {
+    fatal_error(error_message);
+  }
+
+  // Send the request and echo results
+  if (send_request(socket_fd, file_to_send, user, module_num)) {
+    close(socket_fd);
+    fatal_error(NULL);
+  }
+
+  // Close connection
+  close(socket_fd);
+
+  // Change terminal text color back to white
+  printf(KNRM);
+
+  return 0;
+}
---
+++ client.h
@@ -0,0 +1,111 @@
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define VERBOSE 1
+
+#define server_address   "104.199.193.162"
+//#define server_address   "localhost"
+#define port_number       31337
+
+#define INSTR            "instructor"
+#define SOL_SUFFIX       "sol.c"
+#define ARG_DELIM        ":"
+#define HEADER_PREFIX    "FBEGIN"
+#define TCP_PACKET_SIZE   1<<9
+#define MAX_ERROR_SIZE    1<<10
+#define MAX_FILENAME_LEN  1<<7
+#define MAX_HEADER_ELEMS  1<<3
+#define KILOBYTE          1<<10
+
+#define KNRM             "\x1B[00m"
+#define KRED             "\x1B[31m"
+#define KGRN             "\x1B[32m"
+#define KYEL             "\x1B[33m"
+
+#define ACK_LEN 7 
+
+/* Acknowledgment Codes */
+#define CMP_AOK   "CMP_AOK" /* Compilation succeeded            */
+#define CMP_ERR   "CMP_ERR" /* Compilation failed         FATAL */
+#define RUN_AOK   "RUN_AOK" /* A run succeeded                  */
+#define RUN_ERR   "RUN_ERR" /* A run failed               FATAL */
+#define TIM_OUT   "TIM_OUT" /* A run timed out            FATAL */
+#define CHK_AOK   "CHK_AOK" /* A diff succeeded                 */
+#define CHK_ERR   "CHK_ERR" /* A diff failed              FATAL */
+
+#define JDG_ERR   "JDG_ERR" /* Solution not accepted      FATAL */
+#define JDG_AOK   "JDG_AOK" /* Solution accepted                */
+
+#define REQ_AOK   "REQ_AOK" /* Client request received          */
+#define FIL_AOK	  "FIL_AOK" /* File received OK 		*/
+#define FIL_ERR	  "FIL_ERR" /* File received with error		*/
+#define HDR_AOK	  "HDR_AOK" /* Header received OK		*/
+
+#define INV_USR   "INV_USR" /* Invalid username           FATAL */
+#define INV_MOD   "INV_MOD" /* Invalid module number      FATAL */
+#define UNK_ERR   "UNK_ERR" /* Unknown error              FATAL */
+#define BEG_FIL   "BEG_FIL" /* About to stream file             */
+#define BEG_SOL   "BEG_SOL" /* About to stream solution         */
+
+/* Acknowledgement Messages */
+#define REQ_AOK_MSG KGRN "Server succesfully received file\n" KYEL
+#define INV_USR_MSG KRED "Invalid username\n" KYEL
+#define INV_MOD_MSG KRED "Invalid module number\n" KYEL
+#define CMP_AOK_MSG KGRN "Succesfully compiled file\n" KYEL 
+#define CMP_ERR_MSG KRED "Could not compile file using gcc\n" KYEL
+#define RUN_AOK_MSG KGRN "Succesfully ran executable with input file\n" KYEL
+#define RUN_ERR_MSG KRED "There was a runtime error while running with input file\n" KYEL 
+#define CHK_AOK_MSG KGRN "Succesfully passed a test case\n" KYEL
+#define CHK_ERR_MSG KRED "Did not pass a test case\n" KYEL
+#define JDG_AOK_MSG KGRN "Your submission was accepted - great job!\n" KYEL
+#define JDG_ERR_MSG KRED "Your submission was not accepted.\n" KYEL
+#define TIM_OUT_MSG KRED "Submitted program timed out\n" KYEL
+#define BEG_FIL_MSG KYEL "Following message was received from server:\n" KYEL
+#define UNK_ERR_MSG KRED "Unknown error occured while processing request.\nPlease make sure you have complied with assignment requirements.\n" KYEL   
+  
+/*
+** Indicates the state of the connection.
+** Each ack can 
+** 1) not change connection state
+** 2) terminate it
+** 3) send the client into file receiving mode
+*/
+typedef enum {
+  NO_ACTION,
+  TERMINATE,
+  RECEIVE_SOLUTION
+} AckResult;
+
+/*
+** Establishes a TCP connection to server on specified port
+** Returns file descriptor for socket or -1 for error and writes error 
+** error description in the error buffer.
+*/
+int connect_to_server(char *server, int PORT, char *error);
+
+/*
+** If ack is an ack_code, echos its meaning, otherwise echos 
+** the content of the message.
+** Returns an AckResult enum member
+*/
+int handle_ack(char *ack);
+
+/*
+** Sends the request packet {username, module number, source file}
+** to the server over socket_fd. It then waits for the server to send
+** back acknowledgement messages, echoing them as they are received.
+** It halts when server closes the connection.
+** TODO: Break up into 2 loosely coupled routines.
+*/
+int send_request(int socket_fd, char *lfile, char *user, char *module_num); 
+
+/*
+** Receives the solution to the current module from the server
+** Saves the file in the current directory
+*/
+void receive_solution(int socket_fd);
